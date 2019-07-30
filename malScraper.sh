1f08a2661f920a41317d5c42a027ce9c6517c0fe
Ryan Monaghan <monaghanryan95@gmail.com>
2019-07-30 20:13:36 +0100

    Update malScraper.sh

--- malScraper.sh
+++ malScraper.sh
@@ -4,22 +4,22 @@ GRN='\033[0;32m'
 NC='\033[0m' # No Color
 bold=$(tput bold) #variable to format text as bold
 normal=$(tput sgr0) #variable to format text as normal
-#feed='https://urlhaus.abuse.ch/downloads/text/'
 feed=$(base64 -d <<<"H4sIAHp5QF0AA8soKSkottLXLy3KyUgsLdZLTCotTtVLztBPyS/Py8lPTCnWL0mtKNHnAgCoVL2S
 KQAAAA==" | gunzip)
-echo $feed
 timestamp=$(date +%Y-%m-%d-%H:%M)
 clear
 mkdir -p /home/$USER/Desktop/malScraper
-curl -# $feed >> /home/$USER/Desktop/malScraper/PayloadDomains.txt
+curl -# $feed >> /home/$USER/Desktop/malScraper/temp1.txt
 #clear
 cd /home/$USER/Desktop/malScraper/
-cat PayloadDomains.txt | egrep -o "http://([^/]*)/" | sed -e 's/^http:\/\///g' | sed 's/www\./ /g' | sed 's/\/$/ /g' | sed 's/ //g' >> temp.txt
-sort temp.txt | uniq > AMPReport.txt
-rm temp.txt
+cat temp1.txt | egrep -o "http://([^/]*)/" | sed -e 's/^http:\/\///g' | sed 's/www\./ /g' | sed 's/\/$/ /g' | sed 's/ //g' >> temp2.txt
+sort temp1.txt | uniq > PayloadReport.txt
+sort temp2.txt | uniq > AMPReport.txt
+rm temp*
 printf "${GRN}${bold}Success - Files written to:\n${normal}${NC}"
 printf "/home/$USER/Desktop/malScraper/PayloadDomains.txt \n"
 printf "/home/$USER/Desktop/malScraper/AMPReport.txt \n"
 #testing
+#for domain in $(cat "AMPReport.txt"); do dig +short $domain | tr '\n' ' ' >> Blacklist.csv | geoiplookup $domain | awk 'NR==1{print ", " $5,$6,$7,$8}' >> Blacklist.csv ; done
 #logo=$(figlet "eSentire")
 #echo $logo

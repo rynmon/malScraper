0426e2fe6b9b48bb7f452dc080e88eff934a86cb
Ryan Monaghan <monaghanryan95@gmail.com>
2019-08-29 05:51:59 +0100

    Update malScraper.sh

--- malScraper.sh
+++ malScraper.sh
@@ -23,8 +23,13 @@ KQAAAA==" | gunzip)
 C2Feed=$(base64 -d <<<"H4sIAMwQQ10AA0uuTEotSi7KzE3VLSlKTM5OLdLLSy3RT8zJ0SvIKOACAD03/gcfAAAA" | gunzip)
 HexFeed=$(base64 -d <<<"H4sIACgpRF0AA8soKSmw0tcvKUpMzk4t0sswrtBLLdVPLMjUL84sSS2ON8zNzyvJ0CvIKOACAJtI
 bs0rAAAA" | gunzip)
+#Broken :( OpenPhish=$(base64 -d <<<"H4sIACJFXV0AA8soKSkottLXzy9IzSvIyCzO0EvOz9VPS01N0SupKOECAGyI4nkfAAAA" | gunzip)
+PhishTank=$(base64 -d <<<"H4sIAHFCXV0AA8soKSmw0tdPSSxJ1CvIyCzOKEnMy9ZLzs8FC+nn5+Vk5qXqliXmZKboJReXcQEA
+oid3DTAAAAA=" | gunzip)
+HausMalDown=$(base64 -d <<<"H4sIAJqXXl0AA8soKSkottLXLy3KyUgsLdZLTCotTtVLztBPyS/Py8lPTCnWTy4u0+cCAON9198o
+AAAA" | gunzip)
 clear
-mkdir -p /home/$USER/Desktop/malScraper
+mkdir -p /home/$USER/Desktop/malScraper/Phishing
 echo ${arr[$rand]}
 
 #check for presence of existing reports, if found, delete them
@@ -48,6 +53,14 @@ if test -f "$HexReport"; then
 	rm /home/$USER/Desktop/malScraper/HexReport.csv
 	#echo "Updating existing AMP report..."
 fi
+if test -f "$PhishTank"; then
+	rm /home/$USER/Desktop/malScraper/Phishing/PhishTank.csv
+	#echo "Updating existing AMP report..."
+fi
+if test -f "$HausMalDown"; then
+	rm /home/$USER/Desktop/malScraper/HausMalDown.csv
+	#echo "Updating existing AMP report..."
+fi
 
 #sleep 5
 
@@ -55,7 +68,11 @@ printf "#############################################\n# C2 Servers Report sourc
 #pull reports from feeds
 curl -s $C2Feed >> /home/$USER/Desktop/malScraper/C2Report.txt
 curl -s $HexFeed >> /home/$USER/Desktop/malScraper/HexReport.csv
-curl -# $PayloadFeed >> /home/$USER/Desktop/malScraper/PayloadReport.txt
+curl -s $PayloadFeed >> /home/$USER/Desktop/malScraper/PayloadReport.txt
+curl -# $HausMalDown >> /home/$USER/Desktop/malScraper/HausMalDown.csv
+wget -q -P /home/$USER/Desktop/malScraper/Phishing/ $PhishTank 
+cd /home/$USER/Desktop/malScraper/Phishing
+mv online-valid.csv PhishTank.csv
 cd /home/$USER/Desktop/malScraper/
 #strip domains of their http:// and www. headers for ez amp
 cat PayloadReport.txt | egrep -o "http://([^/]*)/" | sed -e 's/^http:\/\///g' | sed 's/www\./ /g' | sed 's/\/$/ /g' | sed 's/ //g' >> AMPReport.txt
@@ -75,15 +92,15 @@ printf "${RED}${bold}C2 Servers:${normal}${NC}"
 printf "/home/$USER/Desktop/malScraper/${bold}C2Report.txt${normal}\n"
 printf "${RED}${bold}Hex Report:${normal}${NC}"
 printf "/home/$USER/Desktop/malScraper/${bold}HexReport.txt${normal}\n"
+printf "${RED}${bold}PhishTank Phishing Pages:${normal}${NC}/home/$USER/Desktop/malScraper/${bold}HausMalDown.csv${normal}\n"
+printf "${RED}${bold}PhishTank Phishing Pages:${normal}${NC}/home/$USER/Desktop/malScraper/Phishing/${bold}PhishTank.csv${normal}\n"
 printf "${RED}${bold}Most Recent 100:${normal}${NC}"
 printf "/home/$USER/Desktop/malScraper/${bold}Top100.txt${normal}\n\n"
-read -n 1 -s -r -p "Press any key to open HexReport..."
+read -n 1 -s -r -p "Press any key to open Malware Domains..."
 printf "\n"
 #prompt user & open report
-xdg-open /home/$USER/Desktop/malScraper/HexReport.csv
+xdg-open /home/$USER/Desktop/malScraper/HausMalDown.csv
 exit 1
-
-
 
 #testing
 #read -n 1 -s -r -p "Press any key to read report..."

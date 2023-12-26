db2cf6a8dc6666a1461fbfa34052b1d7753b4ede
Ryan Monaghan <rynmon@pm.me>
2023-12-26 21:35:34 +0000

    menu imeplementation

--- malScraper.py
+++ malScraper.py
@@ -1,9 +1,33 @@
 import os
 import time
+import sys
+import ctypes
+from pyfiglet import Figlet
+from colorama import Fore, Style
+
+#formatting
+RED=Fore.RED
+GRN=Fore.GREEN
+NC=Style.RESET_ALL
+BOLD=Style.BRIGHT
+NORMAL=Style.NORMAL
+PURPLE=Fore.MAGENTA
+CYAN=Fore.CYAN
+YELLOW=Fore.YELLOW
+
+def displayCountdown(seconds):
+    for i in range(seconds, 0, -1):
+        print(f"Returning to Main Menu in: {i}{' ' if i > 9 else '  '}", end='\r')
+        time.sleep(1)
+
 
 def clearScreen():
     # Clear the screen
-    os.system('cls')
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def clearMenu():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    helpText()
 
 def fullScan():
     print("DEBUG")
@@ -12,13 +36,17 @@ def quickScan():
     print("DEBUG")
 
 def exitApp():
-    print("DEBUG")
+    print("Closing the app...")
+    sys.exit()
 
 def helpFunc():
-    print("DEBUG")
+    helpText()
+    userOptions()
 
 def tutorial():
-    print("DEBUG")
+    print("Tutorial is still on the to-do list.")
+    displayCountdown(3)
+    main()
 
 def reopen():
     print("DEBUG")
@@ -26,27 +54,21 @@ def reopen():
 def installUpdate():
     print("DEBUG")
 
-"""def DEBUGhelpText():
-    print("DEBUG - helpTextMenu")
-
-def DEBUGuserOptions():
-    print("\nDEBUG - userOptionsMenu")"""
-
 def helpText():
-    print("\nHELP MENU :: Available options shown below:\n")
-    print("[*] Tutorial of how to use this tool\t\t\t\t\tTUTORIAL")
-    print("[*] Show this Help Menu\t\t\t\t\t\t\tHELP,GET-HELP,?,-?,/?,MENU")
-    print("[*] Show options for this tool\t\t\t\t\t\tSHOW OPTIONS,SHOW,OPTIONS")
-    print("[*] Clear screen\t\t\t\t\t\t\tCLEAR,CLEAR-HOST,CLS")
-    print("[*] Return to Home Menu\t\t\t\t\t\t\tHOME,BACK,CD ..")
-    print("[*] Open an existing report\t\t\t\t\t\tOPEN,REOPEN")
-    print("[*] Quit malScraper\t\t\t\t\t\t\tQUIT,EXIT")
-    print("[*] Install the latest update\t\t\t\t\t\tINSTALL,UPDATE")
-    print("[*] Perform Full-Scan (Note this may take some time)\t\t\tFULL,FULL-SCAN,FSCAN")
-    print("[*] Perform }Quick-Scan (Most recent 100 Payload Domains)\t\tQUICK,QUICK-SCAN,QSCAN")
+    print(f"\n{CYAN}HELP MENU{NC} :: Available {YELLOW}options{NC} shown below:\n")
+    print(f"{BOLD}[*]{NC} {CYAN}Tutorial{NC} of how to use this tool\t\t\t\t\t{YELLOW}TUTORIAL{NC}")
+    print(f"[*] Show this {CYAN}Help{NC} Menu\t\t\t\t\t\t\t{YELLOW}HELP,GET-HELP,?,-?,/?,MENU{NC}")
+    print(f"[*] {CYAN}Show options{NC} for this tool\t\t\t\t\t\t{YELLOW}SHOW OPTIONS,SHOW,OPTIONS{NC}")
+    print(f"[*] {CYAN}Clear{NC} screen\t\t\t\t\t\t\t{YELLOW}CLEAR,CLEAR-HOST,CLS{NC}")
+    print(f"[*] Return to {CYAN}Home{NC} Menu\t\t\t\t\t\t\t{YELLOW}HOME,BACK,CD ..{NC}")
+    print(f"[*] {CYAN}Open{NC} an existing report\t\t\t\t\t\t{YELLOW}OPEN,REOPEN{NC}")
+    print(f"[*] {CYAN}Quit{NC} malScraper\t\t\t\t\t\t\t{YELLOW}QUIT,EXIT{NC}")
+    print(f"[*] {CYAN}Install{NC} the latest {CYAN}update{NC}\t\t\t\t\t\t{YELLOW}INSTALL,UPDATE{NC}")
+    print(f"[*] Perform {CYAN}Full-Scan{NC} (Note this may take some time)\t\t\t{YELLOW}FULL,FULL-SCAN,FSCAN{NC}")
+    print(f"[*] Perform {CYAN}Quick-Scan{NC} (Most recent 100 Payload Domains)\t\t{YELLOW}QUICK,QUICK-SCAN,QSCAN{NC}")
 
 def userOptions():
-    option=input("malscraper> ").upper()
+    option=input("\nmalscraper> ").upper()
 
     if option in ["FULL", "FULL-SCAN", "FSCAN"]:
         fullScan()
@@ -55,7 +77,7 @@ def userOptions():
     elif option in ["QUIT", "EXIT"]:
         exitApp()
     elif option in ["CLEAR", "CLEAR-HOST", "CLS"]:
-        clearScreen()
+        clearMenu()
     elif option in ["HELP", "GET-HELP", "?", "-?", "/?", "MENU"]:
         helpFunc()
     elif option in ["BACK", "CD ..", "HOME"]:
@@ -68,19 +90,20 @@ def userOptions():
         installUpdate()
     else:
         clearScreen() 
-        print("Error - invalid operation")
+        print(f"{RED}{BOLD}Error: {NORMAL}{NC}Invalid Option.")
         helpText()
         userOptions()
 
 def main():
     clearScreen()
-    print ("---MALSCRAPER PYTHON PORT---")
-    print("\tTool\t :: malScraper")
-    print("\tAuthor\t :: Ryan Monaghan")
-    print("\tEmail\t :: rynmon@pm.me")
-    print("\tGitHub\t :: https://github.com/Ryan-Monaghan/malScraper")
-    print("\tBranch\t :: dev-python")
-    print("\tVersion\t :: 0.1")
+    f = Figlet(font='slant')
+    print(f.renderText('malScraper.py'))
+    print(f"\t{PURPLE}Tool\t :: malScraper{NC}")
+    print(f"\t{PURPLE}Author\t :: Ryan Monaghan{NC}")
+    print(f"\t{PURPLE}Email\t :: rynmon@pm.me{NC}")
+    print(f"\t{PURPLE}GitHub\t :: https://github.com/Ryan-Monaghan/malScraper{NC}")
+    print(f"\t{PURPLE}Branch\t :: dev-python{NC}")
+    print(f"\t{PURPLE}Version\t :: 0.1{NC}")
     helpText()
     userOptions()
 
